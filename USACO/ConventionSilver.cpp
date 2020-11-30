ea0644457d413cb2ccfbd96d8e855d4759aabc09
jicanta <37478735+jicanta@users.noreply.github.com>
2020-11-30 16:18:14 -0300

    Add files via upload

---
+++ USACO/ConventionSilver.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+using tint = long long;
+using ld = long double;
+ 
+#define forsn(i, s, n) for(tint i = s; i < tint(n); i++)
+#define forn(i, n) forsn(i, 0, n)
+#define trav(a, x) for(auto& a : x)
+ 
+using vi = vector<int>;
+using vl = vector<tint>;
+using vb = vector<bool>;
+#define pb push_back
+#define pf push_front
+#define rsz resize
+#define all(x) begin(x), end(x)
+#define rall(x) x.rbegin(), x.rend() 
+#define sz(x) (int)(x).size()
+#define ins insert
+#define esta(x,c) ((c).find(x) != (c).end())
+ 
+using pi = pair<int,int>;
+using pl = pair<tint,tint>;
+#define f first
+#define s second
+#define mp make_pair
+ 
+#define DBG(x) cerr << #x << " = " << x << endl;
+ 
+const tint MOD = 1e9+7;
+const tint mod = 998244353;
+const int MX = 1e8+5; 
+const tint INF = 1e18; 
+const int inf = 2e9;
+const ld PI = acos(ld(-1)); 
+const ld eps = 1e-8;
+ 
+const int dx[4] = {1, -1, 0, 0};
+const int dy[4] = {0, 0, 1, -1};
+ 
+template<class T> void remDup(vector<T> &v){ 
+    sort(all(v)); v.erase(unique(all(v)),end(v));
+}
+ 
+ bool valid(int x, int y, int n, int m){
+    return (0<=x && x<n && 0<=y && y<m);
+}
+ 
+int cdiv(int a, int b) { return a/b+((a^b)>0&&a%b); } //redondea p arriba
+int fdiv(int a, int b) { return a/b-((a^b)<0&&a%b); } //redonde p abajo
+ 
+void NACHO(string name = "convention"){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    freopen((name+".in").c_str(), "r", stdin);
+    freopen((name+".out").c_str(), "w", stdout);
+}
+
+int main(){
+	NACHO();
+	int n, m, c; cin >> n >> m >> c;
+	vi a (n);
+	forn(i, n) cin >> a[i];
+	sort(all(a));
+	int low = 1, high = 1e9+1;
+	while(high-low > 1){
+		int mid = low+(high-low)/2;
+		int M = m-1;
+		int cur = a[0];
+		int tam = 1;
+		forsn(i, 1, n){
+			if(tam >= c || a[i]-cur > mid){
+				tam = 1;
+				cur = a[i];
+				--M;
+			}else ++tam;
+		}
+		if(M < 0){
+			low = mid;
+		} else high = mid;
+	}
+	cout << high << "\n";
+}
