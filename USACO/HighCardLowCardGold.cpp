5ad6900adb8e2d9ccb4e6d1b7eb92c7dd5adc718
jicanta <37478735+jicanta@users.noreply.github.com>
2020-11-30 22:35:40 -0300

    Add files via upload

---
+++ USACO/HighCardLowCardGold.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+using tint = long long;
+using ld = long double;
+ 
+#define forsn(i, s, n) for(tint i = s; i < tint(n); i++)
+#define forn(i, n) forsn(i, 0, n)
+#define trav(a, x) for(auto& a : x)
+ 
+using vi = vector<int>;
+using vl = vector<tint>;
+using vb = vector<bool>;
+#define pb push_back
+#define pf push_front
+#define rsz resize
+#define all(x) begin(x), end(x)
+#define rall(x) x.rbegin(), x.rend() 
+#define sz(x) (int)(x).size()
+#define ins insert
+#define esta(x,c) ((c).find(x) != (c).end())
+ 
+using pi = pair<int,int>;
+using pl = pair<tint,tint>;
+#define f first
+#define s second
+#define mp make_pair
+ 
+#define DBG(x) cerr << #x << " = " << x << endl;
+ 
+const tint MOD = 1e9+7;
+const tint mod = 998244353;
+const int MX = 1e8+5; 
+const tint INF = 1e18; 
+const int inf = 2e9;
+const ld PI = acos(ld(-1)); 
+const ld eps = 1e-8;
+ 
+const int dx[4] = {1, -1, 0, 0};
+const int dy[4] = {0, 0, 1, -1};
+ 
+template<class T> void remDup(vector<T> &v){ 
+    sort(all(v)); v.erase(unique(all(v)),end(v));
+}
+ 
+ bool valid(int x, int y, int n, int m){
+    return (0<=x && x<n && 0<=y && y<m);
+}
+ 
+int cdiv(int a, int b) { return a/b+((a^b)>0&&a%b); } //redondea p arriba
+int fdiv(int a, int b) { return a/b-((a^b)<0&&a%b); } //redonde p abajo
+ 
+void NACHO(string name = "cardgame"){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    freopen((name+".in").c_str(), "r", stdin);
+    freopen((name+".out").c_str(), "w", stdout);
+}
+
+int main(){
+	NACHO();
+	int n; cin >> n;
+	//los n/2 mas grandes los uso en la primera mitad.
+	//Los restantes en la segunda
+	vi a (n);
+	vb seen (2*n+1, 0);
+	forn(i, n) cin >> a[i], seen[a[i]] = 1;
+	vi b;
+	forsn(i, 1, 2*n+1) if(!seen[i]) b.pb(i);
+	set<int> halfA, halfB;
+	sort(all(b));
+	forn(i, n/2) halfB.ins(b[i]);
+	forsn(i, n/2, n) halfA.ins(b[i]);
+	int ret = 0;
+	forn(i, n/2){
+		auto it = halfA.lower_bound(a[i]);
+		if(it != halfA.end()){
+			++ret;
+			halfA.erase(it);
+		}
+	}
+	forsn(i, n/2, n){
+		auto it = halfB.lower_bound(a[i]);
+		if(it != halfB.begin()){
+			--it;
+			++ret;
+			halfB.erase(it);
+		}
+	}
+	cout << ret << "\n";
+}
