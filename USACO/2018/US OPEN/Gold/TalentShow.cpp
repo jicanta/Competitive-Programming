c5fa65ed51d24f66c99e8d7ef4d5f19434c0bd20
jicanta <37478735+jicanta@users.noreply.github.com>
2021-04-09 23:32:54 -0300

    Add files via upload

---
+++ USACO/2018/US OPEN/Gold/TalentShow.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+using tint = long long;
+using ld = long double;
+ 
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define trav(a,x) for (auto& a: x)
+ 
+using pi = pair<int,int>;
+using pl = pair<tint,tint>;
+using vi = vector<int>;
+using vpi = vector<pi>;
+using vpl = vector<pl>;
+using vvi = vector<vi>;
+using vl = vector<tint>;
+using vb = vector<bool>;
+ 
+#define pb push_back
+#define pf push_front
+#define rsz resize
+#define all(x) begin(x), end(x)
+#define rall(x) x.rbegin(), x.rend() 
+#define sz(x) (int)(x).size()
+#define ins insert
+ 
+#define f first
+#define s second
+#define mp make_pair
+ 
+#define DBG(x) cerr << #x << " = " << x << endl;
+ 
+const int MOD = 1e9+7; 
+const tint mod = 998244353;
+const int MX = 805;
+const tint INF = 1e18; 
+const int inf = 2e9;
+const ld PI = acos(ld(-1)); 
+const ld eps = 1e-5;
+ 
+const int dx[4] = {1, -1, 0, 0};
+const int dy[4] = {0, 0, 1, -1};
+ 
+template<class T> void remDup(vector<T> &v){ 
+    sort(all(v)); v.erase(unique(all(v)),end(v));
+}
+ 
+template<class T> bool ckmin(T& a, const T& b) {
+    return b < a ? a = b, 1 : 0; 
+} 
+template<class T> bool ckmax(T& a, const T& b) {
+    return a < b ? a = b, 1 : 0; 
+}
+ 
+bool valid(int x, int y, int n, int m){
+    return (0<=x && x<n && 0<=y && y<m);
+}
+ 
+int cdiv(int a, int b) { return a/b+((a^b)>0&&a%b); } //redondea p arriba
+int fdiv(int a, int b) { return a/b-((a^b)<0&&a%b); } //redondea p abajo
+ 
+void NACHO(string name = "talent"){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    if(sz(name)){
+        freopen((name+".in").c_str(), "r", stdin);
+        freopen((name+".out").c_str(), "w", stdout);
+    }
+}
+
+pl a[255];
+tint dp[1005];
+
+int n, w;
+
+bool can(tint m){
+	F0R(i, w+1) dp[i] = -INF;
+	dp[0] = 0;
+	F0R(i, n){
+		tint val = 1000*a[i].s-m*a[i].f;
+		R0F(j, w+1){
+			int k = min(tint(w), j+a[i].f);
+			ckmax(dp[k], dp[j]+val);
+		}
+	}
+	return dp[w] >= 0;
+}
+
+int main(){
+	NACHO();
+	cin >> n >> w;
+	F0R(i, n) cin >> a[i].f >> a[i].s;
+	tint low = 0, high = 1e9;
+	while(high-low > 1){
+		int mid = low+(high-low)/2;
+		if(can(mid)) low = mid;
+		else high = mid;
+	}
+	cout << low << "\n";
+}
