9860c23bdaedfffb98352b057edbb17d49e7bc39
jicanta <37478735+jicanta@users.noreply.github.com>
2021-04-20 13:20:33 -0300

    Add files via upload

---
+++ USACO/2017/February/Gold/WhyDidTheCowCrossTheRoadIII.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+using tint = long long;
+using ld = long double;
+ 
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define trav(a,x) for (auto& a: x)
+ 
+using pi = pair<int,int>;
+using pl = pair<tint,tint>;
+using vi = vector<int>;
+using vpi = vector<pi>;
+using vpl = vector<pl>;
+using vvi = vector<vi>;
+using vl = vector<tint>;
+using vb = vector<bool>;
+ 
+#define pb push_back
+#define pf push_front
+#define rsz resize
+#define all(x) begin(x), end(x)
+#define rall(x) x.rbegin(), x.rend() 
+#define sz(x) (int)(x).size()
+#define ins insert
+ 
+#define f first
+#define s second
+#define mp make_pair
+ 
+#define DBG(x) cerr << #x << " = " << x << endl;
+ 
+const int MOD = 1e9+7; 
+const tint mod = 998244353;
+const int MX = 1005;
+const tint INF = 1e18; 
+const int inf = 2e9;
+const ld PI = acos(ld(-1)); 
+const ld eps = 1e-5;
+ 
+const int dx[4] = {1, -1, 0, 0};
+const int dy[4] = {0, 0, 1, -1};
+ 
+template<class T> void remDup(vector<T> &v){ 
+    sort(all(v)); v.erase(unique(all(v)),end(v));
+}
+ 
+template<class T> bool ckmin(T& a, const T& b) {
+    return b < a ? a = b, 1 : 0; 
+} 
+template<class T> bool ckmax(T& a, const T& b) {
+    return a < b ? a = b, 1 : 0; 
+}
+ 
+bool valid(int x, int y, int n, int m){
+    return (0<=x && x<n && 0<=y && y<m);
+}
+ 
+int cdiv(int a, int b) { return a/b+((a^b)>0&&a%b); } //redondea p arriba
+int fdiv(int a, int b) { return a/b-((a^b)<0&&a%b); } //redondea p abajo
+ 
+void NACHO(string name = "nocross"){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    if(sz(name)){
+        freopen((name+".in").c_str(), "r", stdin);
+        freopen((name+".out").c_str(), "w", stdout);
+    }
+}
+
+int a[MX], b[MX];
+int dp[MX][MX]; // maximo numero de conexiones
+				// si uso i de a y j de b
+
+int main(){
+	NACHO();
+	int n; cin >> n;
+	F0R(i, n) cin >> a[i];
+	F0R(i, n) cin >> b[i];
+	F0R(i, n+1) F0R(j, n+1) dp[i][j] = -inf;
+	dp[0][0] = 0;
+	F0R(i, n+1){
+		F0R(j, n+1){
+			// no conecto al i
+			if(i) ckmax(dp[i][j], dp[i-1][j]);
+			// no conecto al j
+			if(j) ckmax(dp[i][j], dp[i][j-1]);
+			// lo conecto
+			if(i && j) ckmax(dp[i][j], dp[i-1][j-1]+(abs(a[i-1]-b[j-1]) <= 4));
+		}
+	}
+	cout << dp[n][n] << "\n";
+}
+
