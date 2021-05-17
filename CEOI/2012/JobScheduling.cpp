e42c47c9fcfe6fd24e266bdaf73255e556633a0f
jicanta <37478735+jicanta@users.noreply.github.com>
2021-05-16 23:15:19 -0300

    Add files via upload

---
+++ CEOI/2012/JobScheduling.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+using tint = long long;
+using ld = long double;
+ 
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define trav(a,x) for (auto& a: x)
+ 
+using pi = pair<int,int>;
+using pl = pair<tint,tint>;
+using vi = vector<int>;
+using vpi = vector<pi>;
+using vpl = vector<pl>;
+using vvi = vector<vi>;
+using vl = vector<tint>;
+using vb = vector<bool>;
+ 
+#define pb push_back
+#define pf push_front
+#define rsz resize
+#define all(x) begin(x), end(x)
+#define rall(x) x.rbegin(), x.rend() 
+#define sz(x) (int)(x).size()
+#define ins insert
+ 
+#define f first
+#define s second
+#define mp make_pair
+ 
+#define DBG(x) cerr << #x << " = " << x << endl;
+ 
+const int MOD = 1e9+7;
+const tint mod = 998244353;
+const int MX = 1e5+5;
+const tint INF = 1e18; 
+const int inf = 2e9;
+const ld PI = acos(ld(-1)); 
+const ld eps = 1e-5;
+ 
+const int dx[4] = {1, -1, 0, 0};
+const int dy[4] = {0, 0, 1, -1};
+ 
+template<class T> void remDup(vector<T> &v){ 
+    sort(all(v)); v.erase(unique(all(v)),end(v));
+}
+ 
+template<class T> bool ckmin(T& a, const T& b) {
+    return b < a ? a = b, 1 : 0; 
+} 
+template<class T> bool ckmax(T& a, const T& b) {
+    return a < b ? a = b, 1 : 0; 
+}
+ 
+bool valid(int x, int y, int n, int m){
+    return (0<=x && x<n && 0<=y && y<m);
+}
+ 
+int cdiv(int a, int b) { return a/b+((a^b)>0&&a%b); } //redondea p arriba
+int fdiv(int a, int b) { return a/b-((a^b)<0&&a%b); } //redondea p abajo
+ 
+void NACHO(string name = ""){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    if(sz(name)){
+		freopen((name+".in").c_str(), "r", stdin);
+		freopen((name+".out").c_str(), "w", stdout);
+	}
+}
+ 
+vi a[MX];
+ 
+int main(){
+    NACHO();
+	int n, d, m; cin >> n >> d >> m;
+	F0R(i, m){
+		int x; cin >> x;
+		a[x].pb(i+1);
+	}
+	int low = 0, high = 1e6+5;
+	while(high-low > 1){
+		int mid = low+(high-low)/2;
+		queue<int> q;
+		int maxi = -inf;
+		FOR(i, 1, n+1){
+			F0R(j, sz(a[i])) q.push(i);
+			int Mid = mid;
+			while(sz(q) && Mid){
+				auto u = q.front(); q.pop();
+				--Mid;
+				ckmax(maxi, i-u);
+			}
+		}
+		if(maxi <= d) high = mid;
+		else low = mid;
+	}
+	cout << high << "\n";
+	queue<int> q;
+	FOR(i, 1, n+1){
+		F0R(j, sz(a[i])) q.push(a[i][j]);
+		int Mid = high;
+		while(sz(q) && Mid){
+			auto u = q.front(); q.pop();
+			--Mid;
+			cout << u << " ";
+		}
+		cout << 0 << "\n";
+	}
+}
