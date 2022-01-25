1d4566f054d517cd4ee5706c8651e44b0d3bb3b2
jicanta <37478735+jicanta@users.noreply.github.com>
2022-01-25 16:21:12 -0300

    Add files via upload

---
+++ USACO/2014/February/Gold/CowDecathlon.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template<class T> using Tree = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+ 
+using tint = long long;
+using ld = long double;
+ 
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define trav(a,x) for (auto& a: x)	
+ 
+using pi = pair<int,int>;
+using pl = pair<tint,tint>;
+using vi = vector<int>;
+using vl = vector<tint>;
+using vpi = vector<pi>;
+using vpl = vector<pl>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+ 
+#define pb push_back
+#define pf push_front
+#define rsz resize
+#define all(x) begin(x), end(x)
+#define rall(x) x.rbegin(), x.rend() 
+#define sz(x) (int)(x).size()
+#define ins insert
+ 
+#define f first
+#define s second
+#define mp make_pair
+ 
+#define DBG(x) cerr << #x << " = " << x << endl;
+ 
+const int MOD = 1e9+7; 
+const int mod = 998244353;
+const int MX = 25;
+const tint INF = 1e18; 
+const int inf = 1e9;
+const ld PI = acos(ld(-1)); 
+const ld eps = 1e-5;
+ 
+const int dx[4] = {1, -1, 0, 0};
+const int dy[4] = {0, 0, 1, -1};
+ 
+template<class T> void remDup(vector<T> &v){ 
+	sort(all(v)); v.erase(unique(all(v)),end(v));
+}
+ 
+template<class T> bool ckmin(T& a, const T& b) {
+	return b < a ? a = b, 1 : 0; 
+} 
+template<class T> bool ckmax(T& a, const T& b) {
+	return a < b ? a = b, 1 : 0; 
+}
+ 
+bool valid(int x, int y, int n, int m){
+	return (0<=x && x<n && 0<=y && y<m);
+}
+ 
+int cdiv(int a, int b) { return a/b+((a^b)>0&&a%b); } //redondea p arriba
+int fdiv(int a, int b) { return a/b-((a^b)<0&&a%b); } //redondea p abajo
+ 
+void NACHO(string name = "dec"){
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	if(sz(name)){
+		freopen((name+".in").c_str(), "r", stdin);
+		freopen((name+".out").c_str(), "w", stdout);
+	}
+}
+
+struct Bonus{
+	int p, k, a;
+};
+
+int a[MX][MX], dp[1<<20];
+Bonus b[MX];
+
+int main(){
+	NACHO();
+	int n, m; cin >> n >> m;
+	F0R(i, m){
+		cin >> b[i].k >> b[i].p >> b[i].a;
+	}
+	F0R(i, n) F0R(j, n) cin >> a[i][j];
+	F0R(i, 1<<n) dp[i] = -inf;
+	dp[0] = 0;
+	F0R(ma, 1<<n){
+		F0R(i, n){
+			if(ma&(1<<i)){
+				int id = __builtin_popcount(ma) - 1;
+				int cur = dp[ma^(1<<i)] + a[i][id];
+				F0R(j, m){
+					if(b[j].k == id + 1 && cur >= b[j].p) cur += b[j].a;
+				}
+				ckmax(dp[ma], cur);
+			}
+		}
+	}
+	cout << dp[(1<<n)-1] << "\n";
+}
