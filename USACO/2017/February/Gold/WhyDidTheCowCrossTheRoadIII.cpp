2965a7dc27cda074e81052f8cb69371e3fb71da1
jicanta <37478735+jicanta@users.noreply.github.com>
2022-02-24 21:09:23 -0300

    Add files via upload

--- USACO/2017/February/Gold/WhyDidTheCowCrossTheRoadIII.cpp
+++ USACO/2017/February/Gold/WhyDidTheCowCrossTheRoadIII.cpp
@@ -2,6 +2,10 @@
  
 using namespace std;
  
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template<class T> using Tree = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+ 
 using tint = long long;
 using ld = long double;
  
@@ -9,16 +13,19 @@ using ld = long double;
 #define F0R(i,a) FOR(i,0,a)
 #define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
 #define R0F(i,a) ROF(i,0,a)
-#define trav(a,x) for (auto& a: x)
+#define trav(a,x) for (auto& a: x)	
  
 using pi = pair<int,int>;
 using pl = pair<tint,tint>;
 using vi = vector<int>;
+using vs = vector<string>;
+using vl = vector<tint>;
 using vpi = vector<pi>;
 using vpl = vector<pl>;
 using vvi = vector<vi>;
-using vl = vector<tint>;
+using vvl = vector<vl>;
 using vb = vector<bool>;
+using vvb = vector<vb>;
  
 #define pb push_back
 #define pf push_front
@@ -35,63 +42,83 @@ using vb = vector<bool>;
 #define DBG(x) cerr << #x << " = " << x << endl;
  
 const int MOD = 1e9+7; 
-const tint mod = 998244353;
-const int MX = 1005;
+const int mod = 998244353;
+const int MX = 100005;
 const tint INF = 1e18; 
-const int inf = 2e9;
+const int inf = 1e9;
 const ld PI = acos(ld(-1)); 
-const ld eps = 1e-5;
+const double eps = 1e-9;
  
 const int dx[4] = {1, -1, 0, 0};
 const int dy[4] = {0, 0, 1, -1};
  
 template<class T> void remDup(vector<T> &v){ 
-    sort(all(v)); v.erase(unique(all(v)),end(v));
+	sort(all(v)); v.erase(unique(all(v)),end(v));
 }
  
 template<class T> bool ckmin(T& a, const T& b) {
-    return b < a ? a = b, 1 : 0; 
+	return b < a ? a = b, 1 : 0; 
 } 
 template<class T> bool ckmax(T& a, const T& b) {
-    return a < b ? a = b, 1 : 0; 
+	return a < b ? a = b, 1 : 0; 
 }
  
 bool valid(int x, int y, int n, int m){
-    return (0<=x && x<n && 0<=y && y<m);
+	return (0<=x && x<n && 0<=y && y<m);
 }
  
 int cdiv(int a, int b) { return a/b+((a^b)>0&&a%b); } //redondea p arriba
 int fdiv(int a, int b) { return a/b-((a^b)<0&&a%b); } //redondea p abajo
  
-void NACHO(string name = "nocross"){
-    ios_base::sync_with_stdio(0); cin.tie(0);
-    if(sz(name)){
-        freopen((name+".in").c_str(), "r", stdin);
-        freopen((name+".out").c_str(), "w", stdout);
-    }
+void NACHO(string name = "circlecross"){
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	if(sz(name)){
+		freopen((name+".in").c_str(), "r", stdin);
+		freopen((name+".out").c_str(), "w", stdout);
+	}
 }
 
-int a[MX], b[MX];
-int dp[MX][MX]; // maximo numero de conexiones
-				// si uso i de a y j de b
+int n;
+int a[MX], ft[MX];
+int seen[MX];
+
+int lowbit(int x){
+	return x&(-x);
+}
+
+void upd(int pos, int val){
+	for(int i = pos; i <= n; i += lowbit(i)){
+		ft[i] += val;
+	}
+}
+
+int sum(int x){
+	int s = 0;
+	for(int i = x; i > 0; i -= lowbit(i)){
+		s += ft[i];
+	}
+	return s;
+}
+
+int query(int l, int r){
+	return sum(r) - sum(l - 1);
+}
 
 int main(){
 	NACHO();
-	int n; cin >> n;
+	cin >> n;
+	n *= 2;
 	F0R(i, n) cin >> a[i];
-	F0R(i, n) cin >> b[i];
-	F0R(i, n+1) F0R(j, n+1) dp[i][j] = -inf;
-	dp[0][0] = 0;
-	F0R(i, n+1){
-		F0R(j, n+1){
-			// no conecto al i
-			if(i) ckmax(dp[i][j], dp[i-1][j]);
-			// no conecto al j
-			if(j) ckmax(dp[i][j], dp[i][j-1]);
-			// lo conecto
-			if(i && j) ckmax(dp[i][j], dp[i-1][j-1]+(abs(a[i-1]-b[j-1]) <= 4));
+	int ret = 0;
+	F0R(i, n){
+		if(seen[a[i]] == 0) upd(i+1, 1);
+		if(seen[a[i]] > 0){
+			upd(seen[a[i]], -1);
+			ret += query(seen[a[i]], i+1);
 		}
+		seen[a[i]] = i+1;
 	}
-	cout << dp[n][n] << "\n";
+	cout << ret << "\n";
 }
+ 
 
