bc62a0a5bce7c7b33d27b558e10cbb6a9eee2ec4
jicanta <37478735+jicanta@users.noreply.github.com>
2022-03-07 19:22:42 -0300

    Add files via upload

---
+++ USACO/2019/January/Gold/SleepyCowSorting.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template<class T> using Tree = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+ 
+using tint = long long;
+using ld = long double;
+ 
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define trav(a,x) for (auto& a: x)	
+ 
+using pi = pair<int,int>;
+using pl = pair<tint,tint>;
+using vi = vector<int>;
+using vs = vector<string>;
+using vl = vector<tint>;
+using vpi = vector<pi>;
+using vpl = vector<pl>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+ 
+#define pb push_back
+#define pf push_front
+#define rsz resize
+#define all(x) begin(x), end(x)
+#define rall(x) x.rbegin(), x.rend() 
+#define sz(x) (int)(x).size()
+#define ins insert
+ 
+#define f first
+#define s second
+#define mp make_pair
+ 
+#define DBG(x) cerr << #x << " = " << x << endl;
+ 
+const int MOD = 1e9+7; 
+const int mod = 998244353;
+const int MX = 1e5+5;
+const tint INF = 1e18; 
+const int inf = 1e9;
+const ld PI = acos(ld(-1)); 
+const double eps = 1e-9;
+ 
+const int dx[4] = {1, -1, 0, 0};
+const int dy[4] = {0, 0, 1, -1};
+ 
+template<class T> void remDup(vector<T> &v){ 
+	sort(all(v)); v.erase(unique(all(v)),end(v));
+}
+ 
+template<class T> bool ckmin(T& a, const T& b) {
+	return b < a ? a = b, 1 : 0; 
+} 
+template<class T> bool ckmax(T& a, const T& b) {
+	return a < b ? a = b, 1 : 0; 
+}
+ 
+bool valid(int x, int y, int n, int m){
+	return (0<=x && x<n && 0<=y && y<m);
+}
+ 
+int cdiv(int a, int b) { return a/b+((a^b)>0&&a%b); } //redondea p arriba
+int fdiv(int a, int b) { return a/b-((a^b)<0&&a%b); } //redondea p abajo
+ 
+void NACHO(string name = "sleepy"){
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	if(sz(name)){
+		freopen((name+".in").c_str(), "r", stdin);
+		freopen((name+".out").c_str(), "w", stdout);
+	}
+}
+
+int n;
+int a[MX], ft[MX];
+
+int lowbit(int x) { return x&(-x); }
+
+void upd(int pos){
+	for(int i = pos; i <= n; i += lowbit(i)){
+		++ft[i];
+	}
+}
+
+int sum(int pos){
+	int s = 0;
+	for(int i = pos; i > 0; i -= lowbit(i)){
+		s += ft[i];
+	}
+	return s;
+}
+
+int main(){
+	NACHO();
+	// ans is n - longest sorted suffix.
+	// for each element x, we need to move it 
+	// # of non-sorted elements + # of sorted elements smaller than x
+	cin >> n;
+	F0R(i, n){
+		cin >> a[i];
+	}
+	int ret = 1;
+	R0F(i, n-1){
+		upd(a[i+1]);
+		if(a[i+1] < a[i]){ break; }
+		++ret;
+		
+	}
+	cout << n - ret << "\n";
+	F0R(i, n - ret){
+		if(i > 0) cout << " ";
+		cout << n - ret - i - 1 + sum(a[i]);
+		upd(a[i]);
+	}
+}
+ 
+
