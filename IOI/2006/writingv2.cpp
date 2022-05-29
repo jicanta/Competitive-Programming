4b6a1a528fbf23f929e5a7efad0458637975abc1
Juan Ignacio Cantarella <37478735+jicanta@users.noreply.github.com>
2022-05-29 14:00:10 -0300

    Add files via upload

---
+++ IOI/2006/writingv2.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template<class T> using Tree = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+ 
+using tint = long long;
+using ld = long double;
+ 
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define trav(a,x) for (auto& a: x)	
+ 
+using pi = pair<int,int>;
+using pl = pair<tint,tint>;
+using vi = vector<int>;
+using vpi = vector<pi>;
+using vpl = vector<pl>;
+using vvi = vector<vi>;
+using vl = vector<tint>;
+using vb = vector<bool>;
+ 
+#define pb push_back
+#define pf push_front
+#define rsz resize
+#define all(x) begin(x), end(x)
+#define rall(x) x.rbegin(), x.rend() 
+#define sz(x) (int)(x).size()
+#define ins insert
+ 
+#define f first
+#define s second
+#define mp make_pair
+ 
+#define DBG(x) cerr << #x << " = " << x << endl;
+ 
+const int MOD = 998244353; 
+const int mod = 1e9+7;
+const int MX = 60;
+const tint INF = 1e18; 
+const int inf = 2e9;
+const ld PI = acos(ld(-1)); 
+const ld eps = 1e-5;
+ 
+const int dx[4] = {1, -1, 0, 0};
+const int dy[4] = {0, 0, 1, -1};
+ 
+template<class T> void remDup(vector<T> &v){ 
+    sort(all(v)); v.erase(unique(all(v)),end(v));
+}
+ 
+template<class T> bool ckmin(T& a, const T& b) {
+    return b < a ? a = b, 1 : 0; 
+} 
+template<class T> bool ckmax(T& a, const T& b) {
+    return a < b ? a = b, 1 : 0; 
+}
+ 
+bool valid(int x, int y, int n, int m){
+    return (0<=x && x<n && 0<=y && y<m);
+}
+ 
+int cdiv(int a, int b) { return a/b+((a^b)>0&&a%b); } 
+int fdiv(int a, int b) { return a/b-((a^b)<0&&a%b); } 
+ 
+void NACHO(string name = ""){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    if(sz(name)){
+        freopen((name+".in").c_str(), "r", stdin);
+        freopen((name+".out").c_str(), "w", stdout);
+    }
+}
+
+int freqA[MX], freqB[MX];
+
+bool check(){
+	bool ok = 1;
+	F0R(i, MX) if(freqA[i] != freqB[i]) ok = 0;
+	return ok;
+}
+
+int main(){
+	NACHO();
+	int n, m; cin >> n >> m;
+	string a, b; cin >> a >> b;
+	F0R(i, n) ++freqA[a[i]-'A'];
+	F0R(i, n) ++freqB[b[i]-'A'];
+	int ret = check();
+	FOR(i, n, m){
+		--freqB[b[i-n]-'A'];
+		++freqB[b[i]-'A'];
+		ret += check();
+	}
+	cout << ret << "\n";
+}
+
