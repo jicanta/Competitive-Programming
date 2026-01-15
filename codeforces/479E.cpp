50d929e022000437e2d60708ede9bf0e314f36fa
Juan Ignacio Cantarella <37478735+jicanta@users.noreply.github.com>
2026-01-14 21:35:39 -0300

    Add solution for Codeforces problem 479E

---
+++ codeforces/479E.cpp
@@ -0,0 +1,46 @@
+// https://codeforces.com/problemset/problem/479/E
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MOD = 1e9+7;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+	int n, a, b, k;
+	cin >> n >> a >> b >> k;
+	--a, --b;
+	
+	vector<long long> dp(n+1, 0);
+	// dp[a] -> numero de viajes que terminan en a
+	// hago el truco del "estado implicito" k que serian
+	// cuantos viajes hice
+	dp[a] = 1;
+	
+	for(int i = 0; i < k; ++i) {
+		vector<long long> newDp(n+1, 0);
+		for(int j = 0; j < n; ++j) if(j != b) {
+			int d = abs(j-b);
+			newDp[max(0, j-d+1)] += dp[j];
+			newDp[min(n, j+d)] -= dp[j];
+		}
+		
+		for(int j = 1; j < n; ++j) 
+			newDp[j] = (newDp[j] % MOD + newDp[j-1] % MOD) % MOD;
+		
+		for(int j = 0; j < n; ++j)
+			newDp[j] = ((newDp[j] % MOD - dp[j] % MOD) + MOD) % MOD;
+		dp = newDp;
+		dp[b] = 0;
+	}
+	
+	long long ret = 0;
+	for(int i = 0; i < n; ++i)
+		ret = (ret % MOD + dp[i] % MOD) % MOD;
+		
+	cout << ret << '\n';
+    return 0;
+}
