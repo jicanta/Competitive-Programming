26f822e465d88bf0815102d26801dbbc52549f2c
Juan Ignacio Cantarella <37478735+jicanta@users.noreply.github.com>
2026-01-19 22:05:46 -0300

    Add initial implementation of contest problem M

---
+++ icpc-brazil-2026/day1/M.cpp
@@ -0,0 +1,27 @@
+// el M del contest de Andrew Stankevich
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    
+    for(int i = 0; i < n; ++i)
+		cin >> a[i];
+    
+    for(int i = 0; i < n; ++i) {
+		if(a[i] % 2 != (i+1) % 2) {
+			cout << n-i << '\n';
+			return 0;
+		}
+	}
+	
+	cout << 0 << '\n';
+    return 0;
+}
