01c0832f248946f6ebad1fed1f5dee1abfd3d9d9
Juan Ignacio Cantarella <37478735+jicanta@users.noreply.github.com>
2026-02-05 17:02:37 -0300

    Add solution for AtCoder ABC 442 A

--- atcoder/ABC/442/A.cpp
+++ atcoder/ABC/442/A.cpp
@@ -1,1 +1,25 @@
+#include <bits/stdc++.h>
+#define rep(i, a, b) for (int i = (int)(a); i < (int)(b); i++)
+#define per(i, a, b) for(int i = (int)b-1; i >= a; i--)
+#define sz(x) (int)(x.size())
+#define all(x) begin(x), end(x)
 
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vii = vector<pii>;
+using vl = vector<ll>;
+using vb = vector<bool>;
+
+const int INF = 1'000'000'000;
+const int MOD = 1'000'000'007;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(false);
+	
+	string s; cin >> s;
+	int ret = 0;
+	for(auto c : s) ret += (c == 'i' || c == 'j');
+	cout << ret << '\n';
+}
