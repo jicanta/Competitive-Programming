95c8a4cc454276ee91de9713c8c5d61af2bfc014
Juan Ignacio Cantarella <37478735+jicanta@users.noreply.github.com>
2026-02-05 17:02:59 -0300

    Add B.cpp for AtCoder ABC 442 problem solution

---
+++ atcoder/ABC/442/B.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define rep(i, a, b) for (int i = (int)(a); i < (int)(b); i++)
+#define per(i, a, b) for(int i = (int)b-1; i >= a; i--)
+#define sz(x) (int)(x.size())
+#define all(x) begin(x), end(x)
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vii = vector<pii>;
+using vl = vector<ll>;
+using vb = vector<bool>;
+
+const int INF = 1'000'000'000;
+const int MOD = 1'000'000'007;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(false);
+	
+	int q; cin >> q;
+	int curVol = 0;
+	bool playing = false;
+	rep(i, 0, q) {
+		int t; cin >> t;
+		if(t == 1) curVol++;
+		else if(t == 2) {
+			if(curVol >= 1) curVol--;
+		} else playing ^= 1;
+		cout << ((playing && curVol >= 3) ? "Yes\n" : "No\n");
+	}	
+}
