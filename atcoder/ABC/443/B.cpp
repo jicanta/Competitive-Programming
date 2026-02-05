26914485c25cf8121a85ee200b8f5403b621027f
Juan Ignacio Cantarella <37478735+jicanta@users.noreply.github.com>
2026-02-05 15:25:58 -0300

    Add B.cpp solution for AtCoder ABC 443

---
+++ atcoder/ABC/443/B.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define rep(i, a, b) for (int i = (int)(a); i < (int)(b); i++)
+#define per(i, a, b) for(int i = (int)b-1; i >= a; i--)
+#define sz(x) (int)(x.size())
+#define all(x) begin(x), end(x)
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vii = vector<pii>;
+using vl = vector<ll>;
+using vb = vector<bool>;
+
+const int INF = 1'000'000'000;
+const int MOD = 1'000'000'007;
+
+int main() {
+  cin.tie(0)->sync_with_stdio(false);
+  
+  int n, k; cin >> n >> k;
+  int ret = 0, q = -1;
+  while(ret < k) ret += n++, q++;
+  cout << q << '\n';
+}
