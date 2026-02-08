07483325450368b09c0b5e3b5d6fbbf8bc99bc5e
Juan Ignacio Cantarella <37478735+jicanta@users.noreply.github.com>
2026-02-08 08:33:23 -0300

    Create B.cpp

---
+++ atcoder/ABC/121/B.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define rep(i, a, b) for (int i = (int)(a); i < (int)(b); i++)
+#define per(i, a, b) for(int i = (int)b-1; i >= a; i--)
+#define sz(x) (int)(x.size())
+#define all(x) begin(x), end(x)
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vii = vector<pii>;
+using vl = vector<ll>;
+using vb = vector<bool>;
+using ld = long double;
+
+const int INF = 1'000'000'000;
+const int MOD = 1'000'000'007;
+const ld EPS = 1e-6;
+const int MX = 2e5+5;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(false);
+		
+	int n, m, c; cin >> n >> m >> c;
+	vi b(m); vector<vi> a(n, vi(m));
+	rep(i, 0, m) cin >> b[i];
+	rep(i, 0, n) rep(j, 0, m) cin >> a[i][j];
+	
+	int ret = 0;
+	rep(i, 0, n) {
+		int cur = c;
+		rep(j, 0, m) cur += a[i][j] * b[j];
+		ret += cur > 0;
+	}
+	
+	cout << ret << '\n';
+	
+	return 0;
+}
+
