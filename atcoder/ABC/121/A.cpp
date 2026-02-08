ae70c6fe722bc3ed0acbbf9a788806314c94253e
Juan Ignacio Cantarella <37478735+jicanta@users.noreply.github.com>
2026-02-08 08:22:35 -0300

    Create A.cpp

---
+++ atcoder/ABC/121/A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define rep(i, a, b) for (int i = (int)(a); i < (int)(b); i++)
+#define per(i, a, b) for(int i = (int)b-1; i >= a; i--)
+#define sz(x) (int)(x.size())
+#define all(x) begin(x), end(x)
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vii = vector<pii>;
+using vl = vector<ll>;
+using vb = vector<bool>;
+using ld = long double;
+
+const int INF = 1'000'000'000;
+const int MOD = 1'000'000'007;
+const ld EPS = 1e-6;
+const int MX = 2e5+5;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(false);
+		
+	int H, W, h, w; cin >> H >> W >> h >> w;
+	
+	cout << (H-h)*(W-w) << '\n';
+	
+	return 0;
+}
