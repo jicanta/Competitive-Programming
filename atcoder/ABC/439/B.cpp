6fc7462a499143c37a96472bdc75166b4166897f
Juan Ignacio Cantarella <37478735+jicanta@users.noreply.github.com>
2026-02-13 11:31:31 -0300

    Add solution for AtCoder ABC 439 B problem

---
+++ atcoder/ABC/439/B.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define rep(i, a, b) for (int i = (int)(a); i < (int)(b); i++)
+#define per(i, a, b) for(int i = (int)b-1; i >= a; i--)
+#define sz(x) (int)(x.size())
+#define all(x) begin(x), end(x)
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vii = vector<pii>;
+using vl = vector<ll>;
+using vb = vector<bool>;
+using ld = long double;
+
+const ld INF = 1e18;
+const int MOD = 1000000007;
+const ld EPS = 1e-12;
+const int MX = 2e5+5;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(false);
+	
+	int n; cin >> n;
+	rep(_, 0, 3000) {
+		string s = to_string(n);
+		int newN = 0;
+		rep(i, 0, sz(s)) newN += (s[i]-'0')*(s[i]-'0');
+		n = newN;
+	}
+	cout << (n == 1 ? "Yes\n" : "No\n");
+		
+	return 0;
+}
